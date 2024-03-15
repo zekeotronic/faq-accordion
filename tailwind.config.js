6360109cd426696bb4abec5847ee64d199077ca8
Eric Ihle <zekeotronic@gmail.com>
2024-03-14 22:13:38 -0400

    restructures files tree, installs tailwind and adds og tags to index head

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Work Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
